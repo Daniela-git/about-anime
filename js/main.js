94307236677fdb02f8c1b7dae32de422937d79db
DanielaHA <dhiguitaa17@gmail.com>
2020-06-10 15:27:54 -0500

    se muestra por pantalla los resultados de la busqueda

--- js/main.js
+++ js/main.js
@@ -4,29 +4,46 @@ import * as Api from "/js/api.js";
 const api = new Api.Api();
 let input = document.getElementById("search");
 let button_search = document.getElementById("search_button");
+let inicio_imagenes = document.querySelector(".inicio_imagenes");
 
 // EVENT LISTENERS
 button_search.addEventListener("click", sendSearch);
-input.addEventListener("keypress",key)
+input.addEventListener("keypress", key);
 
 // FUNCIONES
 async function sendSearch(e) {
-    e.preventDefault()
-    let toSearch = getSearch()
-    let animes = await api.search(toSearch)
-    let anime = animes.results[0]
-    // image_url, title, episodios(si no es null), airing, score
-    console.log(anime)
-    
+	e.preventDefault();
+	let toSearch = getSearch();
+	let animes = await api.search(toSearch);
+	let anime = animes.results[0];
+	// image_url, title, episodios(si no es null), airing, score
+	// console.log(anime);
+	showSearchResults(animes.results);
 }
 
 function getSearch() {
-    let toSearch = input.value;
-    return toSearch
+	let toSearch = input.value;
+	return toSearch;
 }
 function key(e) {
-    if (e.key == "Enter"){
-        sendSearch(e)
-    }
-    
+	if (e.key == "Enter") {
+		sendSearch(e);
+	}
+}
+
+function showSearchResults(animes) {
+    let html = ''
+    animes.forEach(anime => {
+        html += `
+        <div class="search_result">
+            <span>${anime.title}</span>
+            <img src="${anime.image_url}" alt="">
+            <ul class="features_anime">
+                <li>episodes: ${anime.episodes}</li>
+                <li>airing: ${anime.airing}</li>
+                <li>score: ${anime.score}</li>
+            </ul>
+        </div>`
+    });
+    inicio_imagenes.innerHTML = html
 }
